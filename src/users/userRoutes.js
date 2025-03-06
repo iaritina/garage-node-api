9cbe493d8c3f967d8e5c52c1c2a77d483195e0d9
iaritina <iaritina7@gmail.com>
2025-03-06 08:19:26 +0300

    api for users registration

---
+++ src/users/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const userService = require("./userService");
+
+router.post("/", async (req, res) => {
+  try {
+    const user = await userService.createUser(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
