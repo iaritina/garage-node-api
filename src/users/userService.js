72d65f12c23f663119def885715beacafbbc3df3
NyHaroavo <razakamahefaharoavo9@gmail.com>
2025-03-06 20:17:04 +0300

    create function login

--- src/users/userService.js
+++ src/users/userService.js
@@ -1,4 +1,7 @@
 const User = require("./userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const createUser = async (user) => {
   try {
@@ -8,6 +11,18 @@ const createUser = async (user) => {
   }
 };
 
+const login = async(email,password) => {
+  const user = await User.findOne({email});
+  if(!user) throw new Error("User not found");
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if(!isMatch) throw new Error("invalid password");
+
+  const token = jwt.sign({_id: user._id, lastname: user.lastname, role: user.role}, process.env.JWT_SECRET, {expiresIn: '1h'});
+  return token;
+}
+
 module.exports = {
   createUser,
+  login,
 };
