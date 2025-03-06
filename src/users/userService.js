2493896783f258cf9db6cb05f51f6339bc0a4c54
iaritina <iaritina7@gmail.com>
2025-03-06 16:10:13 +0300

    hash user's password

--- src/users/userService.js
+++ src/users/userService.js
@@ -1,7 +1,11 @@
 const User = require("./userModel");
+const bcrypt = require("bcrypt");
 
 const createUser = async (user) => {
   try {
+    // le salt est un nombre aléatoire généré pour chaque mot de passe
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
     return await User.create(user);
   } catch (error) {
     throw new Error("Error: " + error.message);
