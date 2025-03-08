8825d728871b9225ac65f4de3b610908fc2d3418
iaritina <iaritina7@gmail.com>
2025-03-08 11:01:38 +0300

    add response to updateBrand method

--- src/vehiculeBrands/brandRoutes.js
+++ src/vehiculeBrands/brandRoutes.js
@@ -28,7 +28,8 @@ router.get("/:id", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   try {
-    res.json(await brandService.updateBrand(req.params.id, req.body));
+    const brand = await brandService.updateBrand(req.params.id, req.body);
+    res.status(200).json(brand);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
