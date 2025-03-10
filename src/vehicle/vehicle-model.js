cc7ecdd48f131c138cfcc61f4caeb58c5627e688
iaritina <iaritina7@gmail.com>
2025-03-10 22:01:53 +0300

    remove required validation for user's attribut

--- src/vehicle/vehicle-model.js
+++ src/vehicle/vehicle-model.js
@@ -1,32 +1,30 @@
 const { Schema } = require("mongoose");
 const mongoose = require("mongoose");
 
-
 const vehicleSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: [true, "User is required"]
-    },
-    model: {
-        type: Schema.Types.ObjectId,
-        ref: "Model",
-        required: [true, "Model is requires"],
-    },
-    immatriculation: {
-        type: String,
-        required: [true, "Immatriculation is requires"],
-        unique: true
-    },
-    year: {
-        type: Number,
-        required: [true, "The year of the vehicle is requiresd"],
-    },
-    isDeleted: {
-        type: Boolean,
-        required: true,
-        default: false
-    }
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  model: {
+    type: Schema.Types.ObjectId,
+    ref: "Model",
+    required: [true, "Model is requires"],
+  },
+  immatriculation: {
+    type: String,
+    required: [true, "Immatriculation is requires"],
+    unique: true,
+  },
+  year: {
+    type: Number,
+    required: [true, "The year of the vehicle is requiresd"],
+  },
+  isDeleted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
 });
 
 const Vehicle = mongoose.model("Vehicle", vehicleSchema);
