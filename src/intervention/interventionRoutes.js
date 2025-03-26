e42db36a55a0a487a3de0d11b4383b13c7036246
iaritina <iaritina7@gmail.com>
2025-03-26 16:43:46 +0300

    fix appointment

--- src/intervention/interventionRoutes.js
+++ src/intervention/interventionRoutes.js
@@ -1,15 +1,16 @@
 const router = require("express").Router();
-const interventionService = require("./intervetionService");
+const interventionService = require("./interventionService");
 
 router.post("/", async (req, res) => {
-    try 
-    {
-        const interventionData = req.body;
-        const intervention  = await interventionService.createIntervention(interventionData);
-        res.status(201).json(intervention);
-    } catch (error) {
-        res.status(500).json({error: error.message});
-    }
+  try {
+    const interventionData = req.body;
+    const intervention = await interventionService.createIntervention(
+      interventionData
+    );
+    res.status(201).json(intervention);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
