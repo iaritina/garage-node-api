65e30e40c25a7609276f83f034b9afe70be95002
iaritina <iaritina7@gmail.com>
2025-03-30 08:54:27 +0300

    add method to get all mechanics

--- src/mechanic/mechanicRoutes.js
+++ src/mechanic/mechanicRoutes.js
@@ -1,34 +1,47 @@
 const router = require("express").Router();
 const mechanic = require("./mechanicService");
 
-
-router.get("/:mechanic/:date", async (req, res) => {
-    try {
-        const response = await mechanic.getAmountOfCommission(req.params.mechanic, req.params.date);
-        res.json(response);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+router.get("/", async (req, res) => {
+  try {
+    const response = await mechanic.getAllMechanics();
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
+router.get("/:mechanic/:date", async (req, res) => {
+  try {
+    const response = await mechanic.getAmountOfCommission(
+      req.params.mechanic,
+      req.params.date
+    );
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 router.get("/vehicle-count/:mechanic/:date", async (req, res) => {
-    try {
-        const response = await mechanic.countRepairedVehicle(req.params.mechanic, req.params.date);
-        res.json(response);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const response = await mechanic.countRepairedVehicle(
+      req.params.mechanic,
+      req.params.date
+    );
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.get("/:mechanic", async (req, res) => {
-    console.log("tonga ato");
-    try {
-        const response = await mechanic.countSerivceByMechanic(req.params.mechanic);
-        res.json(response);
-    } catch (error) {
-        res.status(500).json({error: error});
-    }
+  console.log("tonga ato");
+  try {
+    const response = await mechanic.countSerivceByMechanic(req.params.mechanic);
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 });
 
 module.exports = router;
--- src/mechanic/mechanicService.js
+++ src/mechanic/mechanicService.js
@@ -1,97 +1,102 @@
-const User = require("../users/userModel")
-const Appointment = require("../appointment/appointmentModel")
-const Service = require("../service/serviceModel")
+const User = require("../users/userModel");
+const Appointment = require("../appointment/appointmentModel");
+const Service = require("../service/serviceModel");
+
+const getAllMechanics = async () => {
+  try {
+    const mechanics = await User.find({ role: "mecanicien" });
+    return mechanics;
+  } catch (error) {
+    throw new Error("Error: " + error.message);
+  }
+};
 
 const getAmountOfCommission = async (mechanic, date) => {
-    try {
-        const start = new Date(date);
-        const end = new Date(date);
+  try {
+    const start = new Date(date);
+    const end = new Date(date);
 
-        start.setHours(0,0,0,0);
-        end.setHours(23,59,59,999);
+    start.setHours(0, 0, 0, 0);
+    end.setHours(23, 59, 59, 999);
 
-        const appointments = await Appointment.find({
-            mechanic: mechanic,
-            date: { $gte: start, $lte: end },
-            status: true,
-            isCanceled: false
-        });
+    const appointments = await Appointment.find({
+      mechanic: mechanic,
+      date: { $gte: start, $lte: end },
+      status: true,
+      isCanceled: false,
+    });
 
-        console.log("appointments",appointments);
+    console.log("appointments", appointments);
 
-        let totalCommission = 0;
+    let totalCommission = 0;
 
-        for(const appointment of appointments) {
-            for(const prestation of appointment.prestations) {
-                const service = await Service.findById(prestation.service);
-                console.log("service",service);
-                if(service && service.commission) {
-                    totalCommission += (parseFloat(prestation.price) * service.commission);
-                }
-            }
+    for (const appointment of appointments) {
+      for (const prestation of appointment.prestations) {
+        const service = await Service.findById(prestation.service);
+        console.log("service", service);
+        if (service && service.commission) {
+          totalCommission += parseFloat(prestation.price) * service.commission;
         }
-
-        return totalCommission;
-
-    } catch (error) {
-        throw new Error("Error: ",error.message);
+      }
     }
-}
 
+    return totalCommission;
+  } catch (error) {
+    throw new Error("Error: ", error.message);
+  }
+};
 
 const countRepairedVehicle = async (mechanic, date) => {
-    try {
-        const start = new Date(date);
-        const end = new Date(date);
+  try {
+    const start = new Date(date);
+    const end = new Date(date);
 
-        start.setHours(0,0,0,0);
-        end.setHours(23,59,59,999);
+    start.setHours(0, 0, 0, 0);
+    end.setHours(23, 59, 59, 999);
 
-        const appointments = await Appointment.find({
-            mechanic: mechanic,
-            date: { $gte: start, $lte: end },
-            status: true,
-            isCanceled: false
-        });    
+    const appointments = await Appointment.find({
+      mechanic: mechanic,
+      date: { $gte: start, $lte: end },
+      status: true,
+      isCanceled: false,
+    });
 
-        return appointments.length;
-
-    } catch (error) {
-        throw new Error("Error: ",error.message);
-    }
-    
-}
+    return appointments.length;
+  } catch (error) {
+    throw new Error("Error: ", error.message);
+  }
+};
 
 const countSerivceByMechanic = async (mechanic) => {
-    console.log("mechanic",mechanic);
-    try {
-        const appointments = await Appointment.find({
-            mechanic: mechanic,
-            status: true,
-            isCanceled: false
-        });
-        console.log("appointments",appointments);
+  console.log("mechanic", mechanic);
+  try {
+    const appointments = await Appointment.find({
+      mechanic: mechanic,
+      status: true,
+      isCanceled: false,
+    });
+    console.log("appointments", appointments);
 
-        const serviceCount = {};
+    const serviceCount = {};
 
-        for (const appointment of appointments) {
-            for (const prestation of appointment.prestations) {
-            const service = await Service.findById(prestation.service);
-            if (service) {
-                serviceCount[service.name] = (serviceCount[service.name] || 0) + 1;
-            }
-            }
+    for (const appointment of appointments) {
+      for (const prestation of appointment.prestations) {
+        const service = await Service.findById(prestation.service);
+        if (service) {
+          serviceCount[service.name] = (serviceCount[service.name] || 0) + 1;
         }
-
-        return serviceCount;
-    } catch (error) {
-        throw new Error("Error",error.message);
+      }
     }
-}
 
+    return serviceCount;
+  } catch (error) {
+    throw new Error("Error", error.message);
+  }
+};
 
 module.exports = {
-    getAmountOfCommission,
-    countRepairedVehicle,
-    countSerivceByMechanic
-}
+  getAllMechanics,
+  getAmountOfCommission,
+  countRepairedVehicle,
+  countSerivceByMechanic,
+};
